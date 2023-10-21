7c60cc59066ff7ebfbbcb94e34fac20e61d8f5ea
tsabitaart <146175128+tsabitaart@users.noreply.github.com>
2023-10-21 11:52:18 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan1_php</title>
+</head>
+<body>
+	<h1> Selamat Datang! <h1>
+</body>	
+</html>
---
+++ latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan2_php</title>
+</head>
+<body>
+	<h1> Kalkulator <h1>
+    <form>
+		<label for="angka">Masukkan Angka:</label> 
+		<input type="number" id="angka" name="angka">
+
+		<label for="dropdown">Dropdown</label>
+		<select id="dropdown" name="pilihan">
+			<option value="pilihan1">+</option>
+			<option value="pilihan2">-</option>
+			<option value="pilihan3">*</option>
+		</select>
+		<button type="submit" class="btn btn-primary">Hitung</button>
+	</form>
+</body>
+</html>
