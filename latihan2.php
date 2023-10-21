f310f25602b70b3ddb2d2d5956604c5a31c122ef
tsabitaart <146175128+tsabitaart@users.noreply.github.com>
2023-10-21 11:54:29 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -5,16 +5,20 @@
 </head>
 <body>
 	<h1> Kalkulator <h1>
-    <form>
+    	<form>
+	    <div>
 		<label for="angka">Masukkan Angka:</label> 
 		<input type="number" id="angka" name="angka">
+	    </div>
 
+	    <div>
 		<label for="dropdown">Operasi</label>
 		<select id="dropdown" name="pilihan">
 			<option value="pilihan1">+</option>
 			<option value="pilihan2">-</option>
 			<option value="pilihan3">*</option>
-		</select>
+		</select>    
+	    </div>
 		<button type="submit" class="btn btn-primary">Hitung</button>
 	</form>
 </body>
